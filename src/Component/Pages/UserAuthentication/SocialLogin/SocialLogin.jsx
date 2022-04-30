c534c4688b08b90ed165cf530dee42d70a662797
niiyazobug <niiyaz5555@gmail.com>
2022-04-30 23:07:03 +0600

    facebook signin method and signout method added

--- src/Component/Pages/UserAuthentication/SocialLogin/SocialLogin.jsx
+++ src/Component/Pages/UserAuthentication/SocialLogin/SocialLogin.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import auth from '../../../../Firebase/firebase.init'
 import { useSignInWithFacebook, useSignInWithGoogle, useSignInWithTwitter } from 'react-firebase-hooks/auth';
 import { AiFillGoogleCircle } from 'react-icons/ai';
+import { BsFacebook } from 'react-icons/bs';
+import { useLocation } from 'react-router-dom';
 
 
 const SocialLogin = () => {
@@ -9,17 +11,24 @@ const SocialLogin = () => {
     
     const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth)
 
-    if (googleError) {
+    const [signInWithFacebook, facebookUser, FacebookLoading, FacebookError] = useSignInWithFacebook(auth);
+
+    const location = useLocation()
+    let from = location.state?.from?.pathname || "/";
+
+    if (googleError || FacebookError) {
         return (
           <div>
             <p>Error: {googleError.message}</p>
           </div>
         );
       }
-      if (googleLoading) {
+
+      if (googleLoading || FacebookLoading) {
         return <p>Loading...</p>;
       }
-      if (googleUser) {
+
+      if (googleUser || facebookUser) {
         return (
           <div>
             <p>Signed In User: {googleUser.email}</p>
@@ -36,7 +45,9 @@ const SocialLogin = () => {
                     <button onClick={() => signInWithGoogle()} className=''> <AiFillGoogleCircle className='mt-1 mr-2'></AiFillGoogleCircle> Continue witth google</button>
                 </div>
 
-                
+                <div>
+                    <button onClick={() => signInWithFacebook()} className=' '> <BsFacebook className='mt-1 mr-2'></BsFacebook> Continue witth Facebook</button>
+                </div>
 
             </div>
         </div>
--- src/Component/Shared/Header/Header.js
+++ src/Component/Shared/Header/Header.js
@@ -1,27 +1,30 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import { AiOutlineMenuUnfold } from 'react-icons/ai';
+import auth from '../../../Firebase/firebase.init';
+
 
 
 const Header = () => {
+
+    const [user]=useAuthState(auth)
+
+    const handleSignOut =()=>{
+        signOut(auth)
+    }
+
+
     return (
         <div className=''>
             
-            {/* <nav className="navbar navbar-dark bg-dark">
-                <div className="">
-                    <button className="navbar-toggler" type="button" data-mdb-toggle="collapse"
-                        data-mdb-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <i><AiOutlineMenuUnfold></AiOutlineMenuUnfold></i>
-                    </button>
-                </div>
-            </nav> */}
+           
 
             <div className=' bg-zinc-800 text-white py-3'>
                 <h2 className='text-center'> Fashion Flavour </h2>
             </div>
 
-            {/* <div className="collapse" id="navbarToggleExternalContent"> */}
+           
 
                 <div className="w-28 h-full shadow-md bg-zinc-800 px-1 absolute grid justify-center pt-44">
                     <ul className="relative">
@@ -31,34 +34,19 @@ const Header = () => {
                         <li className="relative my-3">
                             <Link to='/blog' className="flex items-center text-sm py-4 px-7 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:border-b-2 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="dark">Blog</Link>
                         </li>
-                        <li className="relative">
+
+                        {
+                            !user ?
+                            <li className="relative">
                             <Link to='/login' className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:border-b-2 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="dark">LogIn</Link>
                         </li>
+                        :
+                        <li className="relative">
+                            <Link to='/login' onClick={handleSignOut} className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:border-b-2 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="dark">Log Out</Link>
+                        </li>
+                        }
                     </ul>
                 </div>
-
-
-
-
-            {/* </div> */}
-
-
-
-
-            {/* <div className="w-28 h-full shadow-md bg-zinc-800 px-1 absolute grid justify-center pt-44">
-  <ul className="relative">
-    <li className="relative">
-      <Link to='/' className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:border-b-2 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="dark">Home</Link>
-    </li>
-    <li className="relative my-3">
-      <Link to='/blog' className="flex items-center text-sm py-4 px-7 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:border-b-2 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="dark">Blog</Link>
-    </li>
-    <li className="relative">
-      <Link to='/login' className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap hover:border-b-2 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="dark">LogIn</Link>
-    </li>
-  </ul>
-</div> */}
-
 
         </div>
     );
