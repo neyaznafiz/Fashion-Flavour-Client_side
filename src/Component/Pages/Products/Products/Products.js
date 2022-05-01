55fd097ed60a91c72770de6973b175612ddb5f76
niiyazobug <niiyaz5555@gmail.com>
2022-05-02 04:37:31 +0600

    banner component created

---
+++ src/Component/Pages/Products/Products/Products.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useProducts from '../../../Shared/useProducts/useProducts';
+
+const Products = () => {
+
+    const [product, setProduct] = useProducts()
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Products;
