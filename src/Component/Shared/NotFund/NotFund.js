3b67280d41e44bf102f0de66b5aab5cf0254207b
niiyazobug <niiyaz5555@gmail.com>
2022-05-06 17:34:34 +0600

    404 page style done

--- src/Component/Shared/NotFund/NotFund.js
+++ src/Component/Shared/NotFund/NotFund.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const NotFund = () => {
     return (
-        <div className='w-full'>
-            <div className='grid justify-center my-40 h-full'>
-                <div className='w-80 card-shadow grid justify-center text-center'>
+        <div className='w-full pb-28'>
+            <div className='grid justify-center my-40'>
+                <div className='card-shadow grid justify-center text-center px-20 py-20'>
                     <h2 className='text-6xl font-semibold text-red-600'>404</h2>
                     <p className='text-xl'>Your Page is not fund !!!</p>
                 </div>
-                <div>
-                    <Link to='/'> GO TO HOME</Link>
+                <div className='mt-10 flex justify-center'>
+                    <Link to='/' className='card-shadow py-2 px-3 font-semibold hover:text-zinc-800'> GO TO HOME</Link>
                 </div>
             </div>
         </div>
